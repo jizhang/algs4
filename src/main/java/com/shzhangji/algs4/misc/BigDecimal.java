54d8ee2558dbf8bb18c1b780e8123f4409710fd7
Ji ZHANG <jizhang@anjuke.com>
2015-09-10 18:22:32 +0800

    multiply

--- src/main/java/com/shzhangji/algs4/misc/BigDecimal.java
+++ src/main/java/com/shzhangji/algs4/misc/BigDecimal.java
@@ -8,15 +8,8 @@ public class BigDecimal {
     public static String add(String a, String b) {
 
         int length = Math.max(a.length(), b.length()) + 1;
-        int[] arrA = new int[length];
-        int[] arrB = new int[length];
-
-        for (int i = 0; i < a.length(); ++i) {
-            arrA[i] = a.charAt(a.length() - i - 1) - '0';
-        }
-        for (int i = 0; i < b.length(); ++i) {
-            arrB[i] = b.charAt(b.length() - i - 1) - '0';
-        }
+        int[] arrA = stringToArray(a, length);
+        int[] arrB = stringToArray(b, length);
 
         for (int i = 0; i < length; ++i) {
             arrA[i] += arrB[i];
@@ -26,12 +19,51 @@ public class BigDecimal {
             }
         }
 
+        return arrayToString(arrA);
+    }
+
+    public static String multiply(String a, String b) {
+
+        int length = Math.max(a.length(), b.length()) * 2;
+        int[] arrA = stringToArray(a, a.length());
+        int[] arrB = stringToArray(b, b.length());
+        int[] arrC = new int[length];
+
+        for (int i = 0; i < a.length(); ++i) {
+            for (int j = 0; j < b.length(); ++j) {
+                arrC[i + j] += arrA[i] * arrB[j];
+            }
+        }
+
+        for (int i = 0; i < length; ++i) {
+            if (arrC[i] >= 10) {
+                arrC[i + 1] += arrC[i] / 10;
+                arrC[i] = arrC[i] % 10;
+            }
+        }
+
+        return arrayToString(arrC);
+    }
+
+    private static int[] stringToArray(String s, int length) {
+        int[] arr = new int[length];
+        for (int i = 0; i < s.length(); ++i) {
+            arr[i] = s.charAt(s.length() - i - 1) - '0';
+        }
+        return arr;
+    }
+
+    private static String arrayToString(int[] arr) {
+
         int i;
-        for (i = length - 1; arrA[i] == 0; --i);
+        for (i = arr.length - 1; i >= 0 && arr[i] == 0; --i);
+        if (i == -1) {
+            return "0";
+        }
 
         StringBuilder result = new StringBuilder();
         for (; i >= 0; --i) {
-            result.append(arrA[i]);
+            result.append(arr[i]);
         }
 
         return result.toString();
@@ -43,10 +75,14 @@ public class BigDecimal {
         String op = StdIn.readString();
         String b = StdIn.readString();
 
-        String result = "";
+        String result;
         switch (op) {
         case "+":
             result = add(a, b);
+            break;
+
+        case "*":
+            result = multiply(a, b);
             break;
 
         default:
