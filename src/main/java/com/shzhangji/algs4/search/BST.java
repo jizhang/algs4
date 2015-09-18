e0cf202d6352ff6ce272af5085421f5dfa364b02
Ji ZHANG <jizhang@anjuke.com>
2015-09-18 19:58:46 +0800

    bst get & put

---
+++ src/main/java/com/shzhangji/algs4/search/BST.java
@@ -0,0 +1,70 @@
+package com.shzhangji.algs4.search;
+
+public class BST<K extends Comparable<? super K>, V> {
+
+    class Node {
+        K key;
+        V value;
+        Node left, right;
+        int N;
+        Node(K key, V value, int N) {
+            this.key = key;
+            this.value = value;
+            this.N = N;
+        }
+    }
+
+    private Node root;
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        return x == null ? 0 : x.N;
+    }
+
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    private V get(Node x, K key) {
+
+        if (x == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.value;
+        }
+    }
+
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node x, K key, V value) {
+
+        if (x == null) {
+            return new Node(key, value, 1);
+        }
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+}
